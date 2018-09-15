1850b251e7b94bd41c95685c2201a17b21022089
Meriem Abjil <merimari051094@gmail.com>
2018-09-15 13:35:02 +0200

    -

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ app.on('ready', function() {
         height: 600});
 
   //We link the index.html to said window
-  mainWindow.loadUrl('[DIRECTORIO_DEL_FICHERO_INDEX.HTML]');
+  mainWindow.loadUrl('index.html');
 
   //We open the development options
   mainWindow.openDevTools();
