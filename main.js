fca97408044d9f1a55e533b446c3abb644e8004d
Meriem Abjil <merimari051094@gmail.com>
2018-09-15 13:19:18 +0200

    We will define the basic application with Electron framework

---
+++ main.js
@@ -0,0 +1,37 @@
+//Control the application life cycle
+var app = require('app');
+
+//Access the Chromium window gestor
+var BrowserWindow = require('browser-window');
+
+//Allow error report
+require('crash-reporter').start();
+
+//Global eference in the principal window
+var mainWindow = null;
+
+//We will terminate the process of the application when all the windows are closed
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin')
+    app.quit();
+});
+
+//When everything is loaded, with ready event we will start define the application
+app.on('ready', function() {
+  // We will create a new window (800x600)
+  mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600});
+
+  //We link the index.html to said window
+  mainWindow.loadUrl('[DIRECTORIO_DEL_FICHERO_INDEX.HTML]');
+
+  //We open the development options
+  mainWindow.openDevTools();
+
+  // When the main window closes...
+  mainWindow.on('closed', function() {
+    //We release the resources relating to the window
+    mainWindow = null;
+  });
+});
